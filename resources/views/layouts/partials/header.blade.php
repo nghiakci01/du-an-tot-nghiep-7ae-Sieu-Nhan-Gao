847c8a0ae1c9c67e1799888526b6454302809ae8
nghĩa <nghia08092005@gmail.com>
2026-01-28 15:59:53 +0700

    sừa lại nút đăng ký đăng nhập

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -100,22 +100,24 @@
                                 </div>
                                 <div class="top_right text-right">
                                     <ul>
-                                       @guest
-                                           <li><a href="{{ route('login') }}"><i class="ion-android-person"></i> Đăng nhập / Đăng ký</a></li>
-                                       @else
                                            <li class="top_links">
-                                               <a href="#"><i class="ion-android-person"></i> {{ Auth::user()->name }} <i class="ion-chevron-down"></i></a>
+                                               <a href="#"><i class="ion-android-person"></i> My Account <i class="ion-chevron-down"></i></a>
                                                 <ul class="dropdown_links">
-                                                    @if(Auth::user()->isAdmin())
-                                                        <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                                                    <li><a href="#">My Wish List</a></li>
+                                                    <li><a href="{{ route('account.index') }}">My Account</a></li>
+                                                    @auth
+                                                        @if(Auth::user()->isAdmin())
+                                                            <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                                                        @endif
+                                                        <li><a href="#">Compare Products</a></li>
+                                                        <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sign Out</a></li>
+                                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
                                                     @else
-                                                        <li><a href="{{ route('account.index') }}">My Account</a></li>
-                                                    @endif
-                                                    <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Đăng xuất</a></li>
-                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+                                                        <li><a href="{{ route('login') }}">Sign In</a></li>
+                                                        <li><a href="#">Compare Products</a></li>
+                                                    @endauth
                                                 </ul>
-                                            </li> 
-                                       @endguest
+                                            </li>
                                     </ul>
                                 </div>   
 
