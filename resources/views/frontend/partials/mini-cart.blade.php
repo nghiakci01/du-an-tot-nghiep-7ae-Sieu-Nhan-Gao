1ad2fbdeb1c32b0295f53e66eb0e3f86071f83a4
Dong Hoa <donghoamobie@gmail.com>
2026-02-03 22:49:07 +0700

    sửa minicart

--- resources/views/frontend/partials/mini-cart.blade.php
+++ resources/views/frontend/partials/mini-cart.blade.php
@@ -18,24 +18,49 @@
     <!--mini cart-->
     <div class="mini_cart">
         @if(count($cart) > 0)
-            @foreach($cart as $id => $details)
-            <div class="cart_item {{ $loop->first ? 'top' : ($loop->last ? 'bottom' : '') }}">
-                <div class="cart_img">
-                    <a href="{{ route('product.detail', $details['slug']) }}">
-                        <img src="{{ $details['image'] ? asset('storage/' . $details['image']) : asset('frontend-assets/img/s-product/product.jpg') }}" alt="{{ $details['name'] }}">
-                    </a>
+            <div class="cart_items_wrapper" style="max-height: 335px; overflow-y: auto; overflow-x: hidden; padding-right: 5px;">
+                @foreach($cart as $id => $details)
+                <div class="cart_item {{ $loop->first ? 'top' : ($loop->last ? 'bottom' : '') }}">
+                    <div class="cart_img" style="width: 60px; height: 60px; flex: 0 0 60px;">
+                        <a href="{{ route('product.detail', $details['slug']) }}">
+                            <img src="{{ $details['image'] ? asset('storage/' . $details['image']) : asset('frontend-assets/img/s-product/product.jpg') }}" 
+                                 alt="{{ $details['name'] }}" 
+                                 style="width: 60px; height: 60px; object-fit: cover; border: 1px solid #f1f1f1;">
+                        </a>
+                    </div>
+                    <div class="cart_info" style="padding-left: 15px;">
+                        <a href="{{ route('product.detail', $details['slug']) }}" style="font-size: 13px;">{{ Str::limit($details['name'], 25) }}</a>
+                        <span style="font-size: 12px;">{{ $details['quantity'] }}x {{ number_format($details['price']) }} đ</span>
+                    </div>
+                    <div class="cart_remove">
+                        <a href="javascript:void(0)" class="mini-cart-remove" data-id="{{ $id }}">
+                            <i class="ion-android-close"></i>
+                        </a>
+                    </div>
                 </div>
-                <div class="cart_info">
-                    <a href="{{ route('product.detail', $details['slug']) }}">{{ Str::limit($details['name'], 30) }}</a>
-                    <span>{{ $details['quantity'] }}x {{ number_format($details['price']) }} đ</span>
-                </div>
-                <div class="cart_remove">
-                    <a href="javascript:void(0)" class="mini-cart-remove" data-id="{{ $id }}">
-                        <i class="ion-android-close"></i>
-                    </a>
-                </div>
+                @endforeach
             </div>
-            @endforeach
+            
+            <style>
+                .cart_items_wrapper::-webkit-scrollbar {
+                    width: 5px;
+                }
+                .cart_items_wrapper::-webkit-scrollbar-track {
+                    background: #f1f1f1;
+                }
+                .cart_items_wrapper::-webkit-scrollbar-thumb {
+                    background: #ffd541;
+                    border-radius: 5px;
+                }
+                .cart_items_wrapper::-webkit-scrollbar-thumb:hover {
+                    background: #555;
+                }
+                .mini_cart .cart_item {
+                    padding: 10px 0;
+                    display: flex;
+                    align-items: center;
+                }
+            </style>
             
             <div class="cart__table">
                 <table>
