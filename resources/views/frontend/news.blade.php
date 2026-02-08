bd1c545ef41774eff178587d53c4425542c6e6e4
kien <kiendt2105@gmail.com>
2026-02-09 00:32:59 +0700

    language

--- resources/views/frontend/news.blade.php
+++ resources/views/frontend/news.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <!--breadcrumbs area start-->
     <div class="breadcrumbs_area">
-        <div class="container">   
+        <div class="container">
             <div class="row">
                 <div class="col-12">
                     <div class="breadcrumb_content">
@@ -17,18 +17,18 @@
                     </div>
                 </div>
             </div>
-        </div>         
+        </div>
     </div>
     <!--breadcrumbs area end-->
-    
+
     <!--news section area -->
     <div class="blog_page_section mt-60">
-        <div class="container">  
+        <div class="container">
             <div class="row">
                 <div class="col-12">
                     <div class="section_title">
-                        <h2>Tin tức mới nhất</h2>
-                        <p>Cập nhật những xu hướng thời trang và thông báo mới nhất từ Reid Fashion</p>
+                        <h2>{{ __('messages.latest_news') }}</h2>
+                        <p>{{ __('messages.latest_news_desc') }}</p>
                     </div>
                 </div>
             </div>
@@ -44,9 +44,10 @@
                                 <div class="articles_date">
                                     <p>04 Tháng 2, 2026 | <a href="#">Thời trang</a> </p>
                                 </div>
-                                <p class="post_desc">Khám phá những bộ sưu tập mới nhất mang đậm phong cách trẻ trung, năng động cho mùa lễ hội năm nay...</p>
+                                <p class="post_desc">Khám phá những bộ sưu tập mới nhất mang đậm phong cách trẻ trung, năng
+                                    động cho mùa lễ hội năm nay...</p>
                                 <footer class="btn_more">
-                                    <a href="#"> Đọc thêm</a>
+                                    <a href="#"> {{ __('messages.read_more') }}</a>
                                 </footer>
                             </figcaption>
                         </figure>
@@ -63,9 +64,10 @@
                                 <div class="articles_date">
                                     <p>01 Tháng 2, 2026 | <a href="#">Tips & Tricks</a> </p>
                                 </div>
-                                <p class="post_desc">Phong cách Minimalism chưa bao giờ lỗi mốt. Hãy cùng Reid Fashion tìm hiểu cách phối đồ chuẩn "soái ca"...</p>
+                                <p class="post_desc">Phong cách Minimalism chưa bao giờ lỗi mốt. Hãy cùng Reid Fashion tìm
+                                    hiểu cách phối đồ chuẩn "soái ca"...</p>
                                 <footer class="btn_more">
-                                    <a href="#"> Đọc thêm</a>
+                                    <a href="#"> {{ __('messages.read_more') }}</a>
                                 </footer>
                             </figcaption>
                         </figure>
@@ -82,39 +84,41 @@
                                 <div class="articles_date">
                                     <p>25 Tháng 1, 2026 | <a href="#">Thông báo</a> </p>
                                 </div>
-                                <p class="post_desc">Reid Fashion vui mừng thông báo khai trương chi nhánh thứ 5 tại TP. Hồ Chí Minh với nhiều ưu đãi hấp dẫn...</p>
+                                <p class="post_desc">Reid Fashion vui mừng thông báo khai trương chi nhánh thứ 5 tại TP. Hồ
+                                    Chí Minh với nhiều ưu đãi hấp dẫn...</p>
                                 <footer class="btn_more">
-                                    <a href="#"> Đọc thêm</a>
+                                    <a href="#"> {{ __('messages.read_more') }}</a>
                                 </footer>
                             </figcaption>
                         </figure>
                     </article>
                 </div>
             </div>
-        </div>     
+        </div>
     </div>
     <!--news section end-->
 
     <!--newsletter area start-->
     <div class="newsletter_area mt-60 mb-60">
-        <div class="container">   
+        <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-12">
                     <div class="newsletter_text">
-                        <h3>Đăng ký nhận tin</h3>
-                        <p>Nhận ngay thông báo về các bộ sưu tập mới và ưu đãi đặc quyền.</p>
+                        <h3>{{ __('messages.newsletter_signup') }}</h3>
+                        <p>{{ __('messages.newsletter_signup_desc') }}</p>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-12">
                     <div class="newsletter_form">
                         <form id="mc-form" class="mc-form footer-newsletter">
-                            <input id="mc-email" type="email" autocomplete="off" placeholder="Địa chỉ email của bạn..." />
-                            <button id="mc-submit">Đăng ký</button>
+                            <input id="mc-email" type="email" autocomplete="off"
+                                placeholder="{{ __('messages.enter_email') }}" />
+                            <button id="mc-submit">{{ __('messages.subscribe') }}</button>
                         </form>
                     </div>
                 </div>
             </div>
-        </div>    
+        </div>
     </div>
     <!--newsletter area end-->
 @endsection
