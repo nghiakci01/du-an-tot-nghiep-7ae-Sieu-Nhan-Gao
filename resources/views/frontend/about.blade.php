b4c8ddfc1f26a5900096392ea8cdb123ed2f898e
Công Bằng <nguyencongbang20042005@gmail.com>
2026-02-11 22:31:31 +0700

    sửa lại about us

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.public')
 
-@section('title', __('messages.about_us') . ' | Reid Fashion')
+@section('title', __('messages.about_us') . ' | Siêu Nhân Gạo')
 
 @section('content')
     <!--breadcrumbs area start-->
