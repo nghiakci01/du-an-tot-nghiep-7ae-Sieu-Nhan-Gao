7ff632636ab33343b6395bbb976cf755a6afed87
Công Bằng <nguyencongbang20042005@gmail.com>
2026-02-24 00:29:26 +0700

    khi không có banner nào hiện banner sẵn có lúc đầu

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -145,7 +145,7 @@
             <div class="row">
                 <div class="col-lg-8">
                     <div class="slider_area home_six_slider owl-carousel">
-                        @foreach($sliders as $slider)
+                        @forelse($sliders as $slider)
                         <div class="single_slider" data-bgimg="{{ asset('storage/' . $slider->image) }}">
                             <div class="slider_content_inner text-center">
                                 <div class="slider_content">
@@ -158,30 +158,52 @@
                                 </div>
                             </div>
                         </div>
-                        @endforeach
+                        @empty
+                        {{-- Fallback Slider 1 --}}
+                        <div class="single_slider" data-bgimg="{{ asset('frontend-assets/img/slider/slide1.jpg') }}">
+                            <div class="slider_content_inner text-center">
+                                <div class="slider_content">
+                                    <h2>Collection 2026</h2>
+                                    <h1>Elite Minimalism</h1>
+                                    <p>Khám phá phong cách tối giản đỉnh cao và sang trọng.</p>
+                                    <a href="{{ route('shop') }}">{{ __('messages.explore_now') }}</a>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- Fallback Slider 2 --}}
+                        <div class="single_slider" data-bgimg="{{ asset('frontend-assets/img/slider/slide2.jpg') }}">
+                            <div class="slider_content_inner text-center">
+                                <div class="slider_content">
+                                    <h2>New Arrivals</h2>
+                                    <h1>Modern Fashion</h1>
+                                    <p>Cập nhật những xu hướng thời trang mới nhất từ thế giới.</p>
+                                    <a href="{{ route('shop') }}">{{ __('messages.explore_now') }}</a>
+                                </div>
+                            </div>
+                        </div>
+                        @endforelse
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <!--banner area start-->
                     <div class="banner_slider_section">
                         <div class="row">
-                            @if($bannerTop->count() > 0)
-                                @php $firstBanner = $bannerTop->first(); @endphp
-                                <div class="col-12">
-                                    <div class="banner_area banner_top">
-                                        <div class="banner_thumb">
-                                            <a href="{{ $firstBanner->link ?? '#' }}">
-                                                <img src="{{ asset('storage/' . $firstBanner->image) }}" alt="{{ $firstBanner->title }}">
-                                            </a>
-                                            <div class="banner_content text-center">
-                                                <h1>{!! nl2br(e($firstBanner->title)) !!}</h1>
-                                                <h3>{{ $firstBanner->subtitle }}</h3>
+                            @forelse($bannerTop as $banner)
+                                @if($loop->first)
+                                    <div class="col-12">
+                                        <div class="banner_area banner_top">
+                                            <div class="banner_thumb">
+                                                <a href="{{ $banner->link ?? '#' }}">
+                                                    <img src="{{ asset('storage/' . $banner->image) }}" alt="{{ $banner->title }}">
+                                                </a>
+                                                <div class="banner_content text-center">
+                                                    <h1>{!! nl2br(e($banner->title)) !!}</h1>
+                                                    <h3>{{ $banner->subtitle }}</h3>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                
-                                @foreach($bannerTop->skip(1) as $banner)
+                                @else
                                     <div class="col-lg-6 col-md-6 col-sm-6">
                                         <div class="banner_area">
                                             <div class="banner_thumb">
@@ -195,8 +217,47 @@
                                             </div>
                                         </div>
                                     </div>
-                                @endforeach
-                            @endif
+                                @endif
+                            @empty
+                                {{-- Fallback for Top Banners --}}
+                                <div class="col-12">
+                                    <div class="banner_area banner_top">
+                                        <div class="banner_thumb">
+                                            <a href="{{ route('shop') }}">
+                                                <img src="{{ asset('frontend-assets/img/bg/banner1.jpg') }}" alt="Default Banner">
+                                            </a>
+                                            <div class="banner_content text-center">
+                                                <h1>New Collection</h1>
+                                                <h3>Up to 50% Off</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="banner_area">
+                                        <div class="banner_thumb">
+                                            <a href="{{ route('shop') }}">
+                                                <img src="{{ asset('frontend-assets/img/bg/banner2.jpg') }}" alt="Default Banner">
+                                            </a>
+                                            <div class="banner_content text-center">
+                                                <h1>Trending</h1>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="banner_area">
+                                        <div class="banner_thumb">
+                                            <a href="{{ route('shop') }}">
+                                                <img src="{{ asset('frontend-assets/img/bg/banner3.jpg') }}" alt="Default Banner">
+                                            </a>
+                                            <div class="banner_content text-center">
+                                                <h1>Best Seller</h1>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforelse
                         </div>
                     </div>
                     <!--banner area end-->
@@ -274,23 +335,27 @@
     <!--product section area end-->
 
     <!--banner area start-->
-    @if($bannerBottom)
     <div class="banner_slider_section section_fullwidth">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
                     <div class="banner_area">
                         <div class="banner_thumb">
-                            <a href="{{ $bannerBottom->link ?? '#' }}">
-                                <img src="{{ asset('storage/' . $bannerBottom->image) }}" alt="{{ $bannerBottom->title }}">
-                            </a>
+                            @if($bannerBottom)
+                                <a href="{{ $bannerBottom->link ?? '#' }}">
+                                    <img src="{{ asset('storage/' . $bannerBottom->image) }}" alt="{{ $bannerBottom->title }}">
+                                </a>
+                            @else
+                                <a href="{{ route('shop') }}">
+                                    <img src="{{ asset('frontend-assets/img/bg/banner4.jpg') }}" alt="Default Bottom Banner">
+                                </a>
+                            @endif
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @endif
     <!--banner area end-->
 
     <!--product section area start-->
