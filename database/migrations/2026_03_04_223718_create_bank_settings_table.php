888b50834d512b6400a473bcfc8448cbc4c1b1a6
khaccong <congpkph62011@gmail.com>
2026-03-04 22:51:52 +0700

    Fix lỗi khi thêm tk ngân hàng

--- database/migrations/2026_03_04_223718_create_bank_settings_table.php
+++ database/migrations/2026_03_04_223718_create_bank_settings_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('bank_settings', function (Blueprint $table) {
             $table->id();
+            $table->string('bank_name'); // e.g., MB Bank
+            $table->string('bank_id')->nullable(); // Shortcode for VietQR, e.g., MB, VCB
+            $table->string('account_number');
+            $table->string('account_name');
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_default')->default(false); // To mark which one is currently in use
             $table->timestamps();
         });
     }
