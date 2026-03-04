897c1cb44ad5031f1b37c530fe696fbf3e838b7e
khaccong <congpkph62011@gmail.com>
2026-03-05 00:18:20 +0700

    xóa thanh tìm kiếm trong trang admin

--- resources/views/layouts/partials/admin/header.blade.php
+++ resources/views/layouts/partials/admin/header.blade.php
@@ -10,13 +10,7 @@
           <li class="pc-h-item pc-sidebar-popup">
             <a href="#" class="pc-head-link ms-0" id="mobile-collapse"><i class="ti ti-menu-2"></i></a>
           </li>
-          <li class="pc-h-item d-none d-md-inline-flex">
-            <form class="form-search">
-              <i class="search-icon"><svg class="pc-icon">
-                  <use xlink:href="#custom-search-normal-1"></use>
-                </svg> </i><input type="search" class="form-control" placeholder="Ctrl + K" />
-            </form>
-          </li>
+
         </ul>
       </div>
       <!-- [Mobile Media Block end] -->
