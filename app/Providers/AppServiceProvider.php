7a83e0b8ff9c197e7ee95b565080ad285956de5c
Dong Hoa <donghoamobie@gmail.com>
2026-03-05 21:02:05 +0700

    sửa phân trang admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        \Illuminate\Pagination\Paginator::useBootstrapFive();
+
         if ($this->app->environment('production')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
