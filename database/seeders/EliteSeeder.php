8e7eaf2f467031db0fe760e1f70363adf72229e5
nghĩa <nghia08092005@gmail.com>
2026-03-19 23:59:59 +0700

    feat: Add EliteSeeder for database seeding and a script to count active products per category.

--- database/seeders/EliteSeeder.php
+++ database/seeders/EliteSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class elite' extends Seeder
+class EliteSeeder extends Seeder
 {
     /**
      * Run the database seeds.
